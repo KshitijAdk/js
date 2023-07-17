ee7f62d8c733bbe2cfad7ea8e76ef0569d3a7850
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2023-07-17 19:46:19 +0545

    practice

---
+++ script.js
@@ -0,0 +1,20 @@
+// let person = {
+//     name: 'Kshitij',
+//     surname: 'Adhikari',
+//     age: 20,
+//     home: {
+//         permanent: 'Surkhet',
+//         temporary: {
+//             first: 'Anamnagar',
+//             last: 'Sukedhara',
+//         },
+//     },
+//     friends: ['Shahash', 'Swarup']
+// }
+
+// console.log(person)
+// console.log(person.home.temporary.last)
+
+// console.log(person.friends[1])
+
+
