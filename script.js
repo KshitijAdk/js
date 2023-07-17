75ee585972d59312610ad17b3fecb11d3f97527c
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2023-07-17 23:08:21 +0545

    js practices

--- script.js
+++ script.js
@@ -18,3 +18,37 @@
 // console.log(person.friends[1])
 
 
+// let name = prompt('Enter your name: ')
+
+// switch(name){
+//     case 'Kshitij':
+//         console.log("Hello Kshitij, How are you?");
+//         break;
+
+//     case 'Adhikari':
+//         console.log("Hello Adhikari, How are you?")
+//         break;
+
+//     case '':
+//         console.log("Please enter a name")
+// }
+
+
+// let greaterValue;
+
+// if (num1 > num2){
+//     greaterValue = num1;
+// }else{
+//     greaterValue = num2;
+// }console.log(greaterValue)
+
+// Using Ternary Operator
+
+
+
+// let num1 = 10;
+// let num2 = 5;
+// let num3 = 15;
+
+// var greaterValue = (num1 >num2 && num1 > num3)?num1 : (num2>num1 && num2>num3)?num2 : num3;
+// console.log(greaterValue)
