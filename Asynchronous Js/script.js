dbd6eefab184b0268e191a4f8146941cc21e05a0
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2024-03-12 03:39:56 +0545

    asynchronous js

--- Asynchronous Js/script.js
+++ Asynchronous Js/script.js
@@ -79,8 +79,6 @@
 //   }, 1500);
 // }
 
-
-
 // function display(){
 //     task1(() => {
 //         task2(()=>{
@@ -93,3 +91,117 @@
 //     })
 // }
 // display()
+
+// const promise = new Promise(function(resolve, reject){
+//     setTimeout(() => {
+//         // resolve('Its work')
+//         reject(new Error("There are some errors"))
+//     },2000);
+// })
+
+// console.log(promise)
+
+// promise.then(function(result){
+//     console.log(result)
+// }).catch(function(err){
+//     console.log(err.message);
+// })
+
+// function walkDog() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let walkedDog = true
+//         if (walkedDog){
+//             resolve("You walk the dog 🐕");
+//         }else{
+//             reject("You didnt walk the dog.")
+//         }
+//     }, 1500);
+//   });
+// }
+
+// function cleanKitchen() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let cleaned = true
+//         if (cleaned){
+//             resolve("You cleaned the kitchen🧹");
+//         }else{
+//             reject("You didn't cleaned the kitchen")
+//         }
+//     }, 1500);
+//   });
+// }
+
+// function takeOutTrash() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let takeOut = false;
+//         if (takeOut){
+//             resolve("You take out the trash 🚮");
+//         }else{
+//             reject("You didn't take out the trash")
+//         }
+//     }, 1500);
+//   });
+// }
+
+// walkDog()
+//   .then((value) => {
+//     console.log(value);
+//     return cleanKitchen();
+//   })
+//   .then((value) => {
+//     console.log(value);
+//     return takeOutTrash();
+//   })
+//   .then((value) => {
+//     console.log(value);
+//     console.log("You have completed all your work!");
+//   }).catch(error => {
+//     console.error(error)
+//   })
+
+// FETCH WEB API
+
+// const url = "https://dummyjson.com/products/1";
+// const gitAPI = fetch(url);
+
+// gitAPI
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Could not fetch resources");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// PROJECT
+
+async function fetchData() {
+  try {
+    const pokemonName = document
+      .getElementById("pokemonName")
+      .value.toLowerCase();
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resources");
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonSprite");
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
